c789cd5ef64867cfb18ca924a56188aa65db1c72
Matthew Aloba <martmaldigital@gmail.com>
2022-06-24 13:10:28 +0100

    Getting the project stated

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <hi className="text-3xl font-bold underline">getting the project started.</hi>
+    </div>
+  )
+}
+
+export default App
